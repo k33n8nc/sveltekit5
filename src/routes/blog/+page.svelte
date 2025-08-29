6f5d3850c7a83441312ec36ffb9f540dea5083f2
k33n8nc <robbert.stapel@live.nl>
2025-08-29 02:28:02 +0200

    Use Typescript

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { getPosts } from './data.remote';
 </script>
 
--- src/routes/blog/[id]/+page.svelte
+++ src/routes/blog/[id]/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     /** @type {import('./$types').PageProps} */
     let { data } = $props();
 </script>
