64b16d0d0b6405bf1fc1ba5871ff84d9178fb503
k33n8nc <robbert.stapel@live.nl>
2025-08-29 15:55:17 +0200

    Cleanup

--- src/routes/blog/data.remote.ts
+++ src/routes/blog/data.remote.ts
@@ -1,13 +1,17 @@
+import { error } from '@sveltejs/kit';
 import { query } from '$app/server';
 import * as db from '$lib/server/database';
 
 export const getPosts = query(async () => {
-	const posts = await db.sql`
+	const posts = await db.sql
+	`
 		SELECT id, title, slug
 		FROM post
 		ORDER BY published_at
 			DESC
 	`;
-
+	if (!posts){
+		error(404, 'No posts found');
+	}
 	return posts;
-});
+})
